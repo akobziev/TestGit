0fa5d36cb3a18cfc80dec117fcc55a827f2305bf
student <student@D5010>
2018-11-30 21:29:59 +0200

    [add]

--- RegExpresions/Program.cs
+++ RegExpresions/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// https://regexr.com/
+
+using System;
+using System.Text.RegularExpressions;
 
 namespace RegExpresions
 {
@@ -6,7 +9,22 @@ namespace RegExpresions
     {
         static void Main(string[] args)
         {
-            
+            string[] values = { "000-111-22-33", "00-111-22-33" };
+            string text = "000-111-22-33";
+            string pattern = @"^\d{3}-\d{3}-\d{2}-\d{2}$";
+
+            foreach (var value in values)
+            {
+                if (Regex.IsMatch(value, pattern))
+                {
+                    var matched = Regex.Match(value, pattern);
+                    string str = matched.Value;
+                    Console.WriteLine(str);
+                }
+            }
+
+            //var matched = Regex.Matches(text, pattern);
+
         }
     }
 }
